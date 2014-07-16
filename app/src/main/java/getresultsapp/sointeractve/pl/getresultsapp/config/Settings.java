b9beef42a275c46c0ab4d091f890820c93a41f28
pawelDylag <pdylag@sosoftware.pl>
2014-07-16 10:18:02 +0200

    IsaaCloud Connector

---
+++ app/src/main/java/getresultsapp/sointeractve/pl/getresultsapp/config/Settings.java
@@ -0,0 +1,10 @@
+package getresultsapp.sointeractve.pl.getresultsapp.config;
+
+
+public class Settings {
+
+    // connection
+    public static final String instanceId = "157";
+    public static final String appSecret = "59f8d59eb77eaec6f7ef9f1023815550";
+
+}
---
+++ app/src/main/java/getresultsapp/sointeractve/pl/getresultsapp/data/App.java
@@ -0,0 +1,65 @@
+package getresultsapp.sointeractve.pl.getresultsapp.data;
+
+import pl.sointeractive.isaacloud.Isaacloud;
+import android.app.Application;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class App extends Application {
+
+    private static Isaacloud connector;
+    private static App obj;
+    // private static FileManager fileManager;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        obj = this;
+        //fileManager = new FileManager();
+
+    }
+    /*
+    public static void saveUserData(UserData userData) {
+        fileManager.saveUserData(userData, obj);
+    }
+
+    public static UserData loadUserData() {
+        return fileManager.loadUserData(obj);
+    }
+
+    public static void saveLoginData(LoginData data) {
+        fileManager.saveLoginData(data, obj);
+    }
+
+    public static LoginData loadLoginData() {
+        return fileManager.loadLoginData(obj);
+    }
+
+    public static App getInstance() {
+        return obj;
+    }
+
+    public static void resetUserData() {
+        fileManager.resetUserData(obj);
+    }
+    */
+    public static Isaacloud getConnector() {
+        return connector;
+    }
+
+    public static void setConnector(Isaacloud connector) {
+        App.connector = connector;
+    }
+
+    public static boolean isOnline() {
+        ConnectivityManager cm =
+                (ConnectivityManager) obj.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        }
+        return false;
+    }
+
+}
