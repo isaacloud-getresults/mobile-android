54a4d28f37761d70090a85de2d84dd799b0e914f
pawelDylag <pdylag@sosoftware.pl>
2014-07-16 17:35:45 +0200

    Added Toast after login

--- app/src/main/java/getresultsapp/sointeractve/pl/getresultsapp/activities/MainActivity.java
+++ app/src/main/java/getresultsapp/sointeractve/pl/getresultsapp/activities/MainActivity.java
@@ -16,18 +16,8 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(), "hello janusz", Toast.LENGTH_SHORT).show();
+    }
 
-    setContentView(R.layout.activity_main);
-    requestWindowFeature(Window.FEATURE_NO_TITLE);
-    final AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
-    alertDialog.setTitle("Successful registration");
-    alertDialog.setMessage("Hello Janusz");
-    alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
-        public void onClick(DialogInterface dialog, int which) {
-            Toast.makeText(getApplicationContext(), "You clicked OK", Toast.LENGTH_SHORT).show();
-        }
-    });
-    alertDialog.show();
-
-}
 }
