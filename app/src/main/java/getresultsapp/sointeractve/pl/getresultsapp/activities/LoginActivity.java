c8f6192e33e9abd7419729c30099c10035e75a34
pawelDylag <pdylag@sosoftware.pl>
2014-07-16 14:09:53 +0200

    NewUser selection

--- app/src/main/java/getresultsapp/sointeractve/pl/getresultsapp/activities/LoginActivity.java
+++ app/src/main/java/getresultsapp/sointeractve/pl/getresultsapp/activities/LoginActivity.java
@@ -1,6 +1,8 @@
 package getresultsapp.sointeractve.pl.getresultsapp.activities;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,6 +11,8 @@ import android.widget.Button;
 import getresultsapp.sointeractve.pl.getresultsapp.R;
 import getresultsapp.sointeractve.pl.getresultsapp.config.Settings;
 import getresultsapp.sointeractve.pl.getresultsapp.data.App;
+import getresultsapp.sointeractve.pl.getresultsapp.data.LoginData;
+import getresultsapp.sointeractve.pl.getresultsapp.data.UserData;
 import pl.sointeractive.isaacloud.Isaacloud;
 import pl.sointeractive.isaacloud.exceptions.InvalidConfigException;
 
@@ -18,13 +22,19 @@ import java.util.Map;
 
 public class LoginActivity extends Activity {
 
+    private Context context;
+    private LoginData logindata;
+    private UserData userData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        context = this;
+        // create new wrapper instance for API connection
         initializeConnector();
 
+        // find relevant views and add listeners
         Button buttonLogIn = (Button) findViewById(R.id.buttonLogIn);
         buttonLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,10 +49,11 @@ public class LoginActivity extends Activity {
             @Override
             public void onClick(View v) {
                 Log.d("ButtonAction","New user clicked");
+                Intent intent = new Intent(context, RegisterActivity.class);
+                startActivity(intent);
             }
 
         });
-
     }
 
     public void initializeConnector() {
