f34c1ec44cd90db169eb4b64dfa89803db2e055b
pawelDylag <pdylag@sosoftware.pl>
2014-07-17 14:47:59 +0200

    Deleted debug login

--- app/src/main/java/getresultsapp/sointeractve/pl/getresultsapp/activities/MainActivity.java
+++ app/src/main/java/getresultsapp/sointeractve/pl/getresultsapp/activities/MainActivity.java
@@ -1,14 +1,14 @@
 package getresultsapp.sointeractve.pl.getresultsapp.activities;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
-import android.content.DialogInterface;
+
 
 import getresultsapp.sointeractve.pl.getresultsapp.R;
+import getresultsapp.sointeractve.pl.getresultsapp.data.App;
 
 public class MainActivity extends Activity {
     
@@ -16,22 +16,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
-    
-    public void onClick(View v) {
-		showDialog(0);
-	}
-	/*
-   protected Dialog onCreateDialog(int id) {
-		switch(id) {
-		case 0:
-			return new AlertDialog.Builder(this).setIcon(R.drawable.ic_launcher).setTitle("Hello Janusz").setPositiveButton("OK", new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int whichButton) {
-					Toast.makeText(getBaseContext(), "OK clicked!", Toast.LENGTH_SHORT).show();
-				}
-			}
-		}
-		return null;
-	}
-    */
+
+
 }
